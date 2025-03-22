2d45770755e6eef944f7f5437e2b392aa32c78a6
Shannon Hochkins <mail@shannonhochkins.com>
2025-03-22 23:38:20 +1100

    Cleanup

--- packages/editor/src/lib/editor/puck/dynamic-puck-configuration.ts
+++ packages/editor/src/lib/editor/puck/dynamic-puck-configuration.ts
@@ -1,80 +1,17 @@
 import { type UserConfig } from '@typings/puck';
 import { ComponentFactoryData, CustomComponentConfig } from '../components';
 import { ComponentConfig, DefaultComponentProps } from '@measured/puck';
-import { init, loadRemote, preloadRemote, registerPlugins } from '@module-federation/enhanced/runtime';
-
-import type { FederationRuntimePlugin } from '@module-federation/enhanced/runtime';
+import { init, loadRemote, preloadRemote } from '@module-federation/enhanced/runtime';
 import { createComponent } from './createComponent';
 
-interface RemotePlugin {
-  name: string;
-  exposes: {
-    moduleName: string;
-    modulePath?: string;
-  }
-}
-
-
-
-// const runtimePlugin: () => FederationRuntimePlugin = function () {
-//   return {
-//     name: 'generate-dynamic-component-list',
-//     async generatePreloadAssets(args) {
-//       console.log('generatePreloadAssets:', args);
-//       // this is the only way i was able to find the remote information
-//       // before actually loading it
-//       return {
-//         cssAssets: [],
-//         jsAssetsWithoutEntry: [],
-//         entryAssets: [],
-//       }
-//     },
-//   };
-// };
-// registerPlugins([runtimePlugin()]);
-
-// import { createComponent } from '@editor/components';
-// import { getComponentsForUser } from '../../api/component';
-// import { useFederation } from '../fed';
-// import { init, loadRemote } from '@module-federation/enhanced/runtime';
-
-
 interface ComponentModule {
-  default: (data: ComponentFactoryData) => Promise<CustomComponentConfig<DefaultComponentProps>>;
+  default: CustomComponentConfig<DefaultComponentProps>;
 }
 
 export async function getPuckConfiguration(data: ComponentFactoryData) {
-  const remotePlugins: RemotePlugin[] = [];
-  // const modules = import.meta.glob<ComponentModule>('../components/*/index.tsx');
   const components: UserConfig['components'] = {};
   let rootConfig: UserConfig['root'] | undefined;
   const categories: NonNullable<UserConfig['categories']> = {} as NonNullable<UserConfig['categories']>;
-  // THIS DOESN"T WORK, need the manifest after preload somehow
-  // const remotePlugin: () => FederationRuntimePlugin = function () {
-  //   return {
-  //     name: 'generate-dynamic-component-list',
-  //     async generatePreloadAssets(args) {
-  //       // forces types to use ConsumerModuleInfo with this guard
-  //       if ('publicPath' in args.remoteSnapshot) {
-  //         console.log('x')
-  //         remotes.push({
-  //           name: args.remote.name,
-  //           exposes: args.remoteSnapshot.modules.map(mod => ({
-  //             moduleName: mod.moduleName,
-  //             modulePath: mod.modulePath
-  //           }))
-  //         });
-  //       }
-  //       // this is the only way i was able to find the remote information
-  //       // before actually loading it
-  //       return {
-  //         cssAssets: [],
-  //         jsAssetsWithoutEntry: [],
-  //         entryAssets: [],
-  //       }
-  //     },
-  //   };
-  // };
 
   const remotes = [
     {
@@ -87,7 +24,6 @@ export async function getPuckConfiguration(data: ComponentFactoryData) {
   const host = init({
     name: '@hakit/editor',
     remotes: remotes,
-    // plugins: [remotePlugin()],
     shareStrategy: 'loaded-first',
   });
 
@@ -102,9 +38,7 @@ export async function getPuckConfiguration(data: ComponentFactoryData) {
     // type guard to ensure we have the correct type when iterating modules
     if ('publicPath' in snapshot.remoteSnapshot) {
       for (const module of snapshot.remoteSnapshot.modules) {
-        const component = await loadRemote<{
-          default: CustomComponentConfig<DefaultComponentProps>;
-        }>(`${remote.name}/${module.moduleName}`).then(loadedModule => {
+        const component = await loadRemote<ComponentModule>(`${remote.name}/${module.moduleName}`).then(loadedModule => {
           if (!loadedModule) {
             throw new Error(`No "${module.moduleName}" component found`);
           }
